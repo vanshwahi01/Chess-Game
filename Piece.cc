700e148f6ee0e46f53299c5ce48a611d88840e29
Vykiial <109167832+Vykiial@users.noreply.github.com>
2023-12-02 12:44:36 -0500

    Updated Piece and started Pawn

--- Piece.cc
+++ Piece.cc
@@ -3,13 +3,27 @@
 
 using namespace std;
 
-Piece::Piece(Coordinates c, Colour colour) : coords{c}, colour{colour} {}
+Piece::Piece(Coordinates c, Colour colour) : coords{c}, colour{colour}, numMoves{0} {}
 
 vector<Coordinates> Piece::possibleMoves(const Board& b, const Piece& p, const Coordinates& c) {}
 
-void Piece::move(const Coordinates& c) {}
+void Piece::move(const Coordinates& c, const Board b) {
+    
+}
 
 Piece::~Piece() {}
+
+void Piece::getNumMoves() {
+    return numMoves;
+}
+
+void Piece::getColour() {
+    return colour;
+}
+
+void Piece::getCoords() {
+    return coords;
+}
 
 Board::Board() : x{0} {}
 
--- Piece.h
+++ Piece.h
@@ -23,13 +23,20 @@ enum Colour {Black, White};
 class Piece {
     Coordinates coords;
     Colour colour;
+    int numMoves;
 
 public:
     Piece(Coordinates c, Colour colour);
 
     virtual vector<Coordinates> possibleMoves(const Board& b, const Piece& p, const Coordinates& c); // Returns a vector of availble moves
 
-    virtual void move(const Coordinates& c); // Moves the current piece and throws an exception if the move is illegal
+    virtual void move(const Coordinates& c, const Board b); // Moves the current piece and throws an exception if the move is illegal
 
+    void getNumMoves(); // Returns numMoves
+
+    void getColour(); // Returns colour
+
+    void getCoords(); // Returns coords
+    
     virtual ~Piece() = 0;
 };
