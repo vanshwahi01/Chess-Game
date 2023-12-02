552748e883428dd1091ae05d50d76cc8941f18dd
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2023-12-02 13:11:47 -0500

    Updated player.h

--- player.h
+++ player.h
@@ -4,44 +4,21 @@
 #include <vector>
 using namespace std;
 
+enum Colour {White = 0, Black = 1};
+
 class Player {
+        vector<Pieces*> (16);
 
-        King king;
-        Queen queen;
-        Rook r1, r2;
-        Bishop b1, b2;
-        Knight k1, k2;
-        Pawn p1, p2, p3, p4, p5, p6, p7, p8;
-        //alternatively
-        //vector<Pawn> p(8);
-
-        int colour = 0; // 0 is black, 1 is white
+        Colour c = Colour::White;
         int score = 0;
         int totalPieces = 0;
         public:
-
                 Player();
                 int getScore();
                 int getColour();
                 void incScore();
                 int getTotalPieces();
 
-
-
-
 };
-
-class Computer : public Player {
-        int level = 1;
-        vector<Pieces> p(16);
-
-        public:
-                Computer(int level);
-                int getLevel();
-                Pieces randomPiece(); //returns a random piece
-                //get a random index
-                //the piece at that random index will be the piece that is chosen to be played
-
-}
 
 #endif
