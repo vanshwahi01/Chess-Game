887ecf56788f99053b7d17bd13cd50781366d1f5
Vykiial <109167832+Vykiial@users.noreply.github.com>
2023-12-03 14:40:55 -0500

    Finished Pawn and updated piece

--- Pawn.cc
+++ Pawn.cc
@@ -5,33 +5,51 @@ using namespace std;
 
 Pawn::Pawn(Coordinate c, Colour colour, Board b, string type) : Piece{c, colour, b, type} {}
 
-void Pawn::promote(const Board& b, Piece& p) {
-    Coordinate = p.getCoords();
+void Pawn::promote(const Board& b, Piece& p) { // This is gonna have to be a function in Board or main
+    Coordinate = p.getCoords(); // Set aside the pawns coordinates
 
     for(auto i: b->pieces) {
-        if(i.getCoords() == p.getCoords()) 
+        if(i.getCoords() == p.getCoords()) // Look for the pawn
     }
 }
 
 vector<Coordinate> Pawn::possibleMoves(const Board& b, const Piece& p, const Coordinate& c) {
     vector<Coordinates> pMoves[0];
-    if(!b->getPiece({c->x, c->y + 1})->isOccupied()) { // Check if the pawn can move forward
+    if(!b->isOccupied({c->x, c->y + 1})) { // Check if the pawn can move forward
         pMoves.push_back({c->x, c->y + 1});
     }
 
-    if(c->x > 0 && b->getPiece({c->x - 1, c->y + 1})->isOccupied()) { // Check if left diagnol is occupied
+    if(c->x > 0 && b->isOccupied({c->x - 1, c->y + 1})) { // Check if left diagnol is occupied
         if(b->getPiece({c->x - 1, c->y + 1})->colour != p->colour) { // Check if it is opposing team's piece
             pMoves.push_back({c->x - 1, c->y + 1});
         }
     }
 
-    if(c->x < board.length - 1 && b->getPiece({c->x + 1, c->y + 1})->isOccupied()) { // Check if right diagnol is occupied
+    if(c->x < board.length - 1 && b->isOccupied({c->x + 1, c->y + 1})) { // Check if right diagnol is occupied
         if(b->getPiece({c->x + 1, c->y + 1})->colour != p->colour) { // Check if it is opposing team's piece
             pMoves.push_back({c->x + 1, c->y + 1});
         }
     }
-    // Add checks for enPassant and double move at beginning
+
+    if(numMoves == 0 && !(b->isOccupied({c->x, c->y + 1})) && !(b->isOccupied({c->x, c->y + 2}))) {
+        // Check if both space directly in front and two in front of pawn are free and that the pawn hasn't moved
+        pMoves.push_back({c->x, c->y + 2}); // Add the space two spaces in front of pawn
+    }
+
+    if(b->isOccupied({c->x - 1, c->y}) && b->getPiece({c->x - 1, c->y}).getColour() != p->getColour()) { // Left side en passant check
+        // Will enter statement if there is a piece on the left side of the pawn of the opposing colour
+        if(b->getPiece({c->x - 1, c->y}).getType() == "Pawn" && b->getPiece({c->x - 1, c->y}).getNumMoves() == 1) {
+            // Will enter statement if the piece is a pawn and it has only made one move
+            pMoves.push_back({c->x - 1, c->y});
+        }
+    }
+
+    if(b->isOccupied({c->x + 1, c->y}) && b->getPiece({c->x + 1, c->y}).getColour() != p->getColour()) { // Right side en passant check
+        // Will enter statement if there is a piece on the left side of the pawn of the opposing colour
+        if(b->getPiece({c->x + 1, c->y}).getType() == "Pawn" && b->getPiece({c->x + 1, c->y}).getNumMoves() == 1) {
+            // Will enter statement if the piece is a pawn and it has only made one move
+            pMoves.push_back({c->x + 1, c->y});
+        }
+    }
     return pMoves;
 }
- 
-// To check if king is checked, call possibleMoves() on every opposing piece and iterate through given vectors to see if king's coordinates are in given vector
--- Piece.cc
+++ Piece.cc
@@ -30,8 +30,19 @@ string Piece::getType() {
 }
 
 bool Piece::isLegal(const Coordinate& c) {
+    for(auto p: curBoard->getPieces()) {
+        if(p->getType() == "King" && p->getColour == Colour::White) {
+            Coordinate whiteK = p->getCoords(); // Keep track of white king's coords
+        }
+        if(p->getType() == "King" && p->getColour == Colour::Black) {
+            Coordinate blackK = p->getCoords(); // Keep track of white king's coords
+        }
+    }
+
     for(auto p: possibleMoves(curBoard, *this, this->getCoords())) { // Checks to see if c is a legal move
-        if(p == c) return true;
+        if(p == c && (p != whiteK || p != blackK) return true; // Make sure to double check this line
+        // Line above should look for a matching coordinate in the given vector and make sure it isn't equal to 
+        // position of either king
     }
     return false;
 }
