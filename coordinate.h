078896ecb0ed7dba3d946e7f412ab6d9c4b84fba
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2023-12-03 20:35:23 -0500

    Create coordinate.h

---
+++ coordinate.h
@@ -0,0 +1,12 @@
+#ifndef COORDINATE_H
+#define COORDIATE_H
+using namespace std;
+
+class Coordinate {
+        public:
+                int x = 0;
+                int y = 0;
+                operator==(const Coordinates& other);
+};
+
+#endif
