0254592121a2da928b3ef6b8c02b6f059d507a28
Vansh <124472925+vanshwahi01@users.noreply.github.com>
2023-12-05 02:09:18 -0500

    Update Rook.cc

--- Rook.cc
+++ Rook.cc
@@ -1,66 +1,67 @@
 #include <vector>
 #include "Piece.h"
+#include "Rook.h"
 
 using namespace std;
 
 Rook::Rook(Coordinate c, Colour colour, Board b, string type) : Piece{c, colour, b, type} {}
 
-vector<Coordinate> Rook::possibleMoves(const Board& b, const Piece& p, const Coordinate& c) {
-    vector<Coordinates> pMoves[0];
+vector<Coordinate> Rook::possibleMoves(Board& b, Piece& p, Coordinate& c) {
+    vector<Coordinate> pMoves(0);
     
-    for(int y = p->getCoords().y + 1; y < 7; y++) { // Forward Straight
-        if(b->isOccupied({c->x, y}) && b->getPiece({c->x, y}).getColour() == p->getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
+    for(int y = p.getCoords().y + 1; y < 7; y++) { // Forward Straight
+        if(b.isOccupied({c.x, y}) && b.getPiece({c.x, y})->getColour() == p.getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
 
-        if(b->isOccupied({c->x, y}) && b->getPiece({c->x, y}).getColour() != p->getColour()) {
+        if(b.isOccupied({c.x, y}) && b.getPiece({c.x, y})->getColour() != p.getColour()) {
             // If the piece blocking the path is the opposite colour, add it to the vector and break
-            pMoves.push_back({c->x, y});
+            pMoves.push_back({c.x, y});
             break;
         }
 
-        if(!b->isOccupied({c->x, y})) pMoves.push_back({c->x, y}); // If space is empty, add it to vector
+        if(!b.isOccupied({c.x, y})) pMoves.push_back({c.x, y}); // If space is empty, add it to vector
     }
 
-    for(int y = p->getCoords().y - 1; y > - 1; y--) { // Backwards straight
-        if(b->isOccupied({c->x, y}) && b->getPiece({c->x, y}).getColour() == p->getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
+    for(int y = p.getCoords().y - 1; y > - 1; y--) { // Backwards straight
+        if(b.isOccupied({c.x, y}) && b.getPiece({c.x, y})->getColour() == p.getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
 
-        if(b->isOccupied({c->x, y}) && b->getPiece({c->x, y}).getColour() != p->getColour()) {
+        if(b.isOccupied({c.x, y}) && b.getPiece({c.x, y})->getColour() != p.getColour()) {
             // If the piece blocking the path is the opposite colour, add it to the vector and break
-            pMoves.push_back({c->x, y});
+            pMoves.push_back({c.x, y});
             break;
         }
 
-        if(!b->isOccupied({c->x, y})) pMoves.push_back({c->x, y}); // If space is empty, add it to vector
+        if(!b.isOccupied({c.x, y})) pMoves.push_back({c.x, y}); // If space is empty, add it to vector
     }
 
-    for(int x = p->getCoords().x - 1; x > -1; x--) { // Left straight
-        if(b->isOccupied({x, c->y}) && b->getPiece({x, c->y}).getColour() == p->getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
+    for(int x = p.getCoords().x - 1; x > -1; x--) { // Left straight
+        if(b.isOccupied({x, c.y}) && b.getPiece({x, c.y})->getColour() == p.getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
 
-        if(b->isOccupied({x, c->y}) && b->getPiece({x, c->y}).getColour() != p->getColour()) {
+        if(b.isOccupied({x, c.y}) && b.getPiece({x, c.y})->getColour() != p.getColour()) {
             // If the piece blocking the path is the opposite colour, add it to the vector and break
-            pMoves.push_back({x, c->y});
+            pMoves.push_back({x, c.y});
             break;
         }
 
-        if(!b->isOccupied({x, c->y})) pMoves.push_back({x, c->y}); // If space is empty, add it to vector
+        if(!b.isOccupied({x, c.y})) pMoves.push_back({x, c.y}); // If space is empty, add it to vector
     }
 
-    for(int x = p->getCoords().x + 1; x < 7; x++) { // Right straight
-        if(b->isOccupied({x, c->y}) && b->getPiece({x, c->y}).getColour() == p->getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
+    for(int x = p.getCoords().x + 1; x < 7; x++) { // Right straight
+        if(b.isOccupied({x, c.y}) && b.getPiece({x, c.y})->getColour() == p.getColour()) break; // Stop checking the diagonal once there is a piece that is your colour
 
-        if(b->isOccupied({x, c->y}) && b->getPiece({x, c->y}).getColour() != p->getColour()) {
+        if(b.isOccupied({x, c.y}) && b.getPiece({x, c.y})->getColour() != p.getColour()) {
             // If the piece blocking the path is the opposite colour, add it to the vector and break
-            pMoves.push_back({x, c->y});
+            pMoves.push_back({x, c.y});
             break;
         }
 
-        if(!b->isOccupied({x, c->y})) pMoves.push_back({x, c->y}); // If space is empty, add it to vector
+        if(!b.isOccupied({x, c.y})) pMoves.push_back({x, c.y}); // If space is empty, add it to vector
     }
-
-    if(c->x - 2 > -1 && !b->isOccupied({c->x - 1, y}) && !b->isOccupied({c->x - 2, y}) && p->getNumMoves() == 0) { // Check for castling
-        for(auto i: b->getPieces()) { // Looking for the correct king
-            if(i->getType == "King" && i->getColour() == p->getColour()) { // Find the correct King
-                if(i->getNumMoves() == 0 && !i.isChecked(b) && i->getCoords() == {c->x - 3, c->y}) { // Check if it has moved yet
-                    pMoves.push_back({{c->x - 2, y}});
+    // y is undefined here?? line 60
+    if(c.x - 2 > -1 && !b.isOccupied({c.x - 1, y}) && !b.isOccupied({c.x - 2, y}) && p.getNumMoves() == 0) { // Check for castling
+        for(auto i: b.getPieces()) { // Looking for the correct king
+            if(i->getType() == "King" && i->getColour() == p.getColour()) { // Find the correct King
+                if(i->getNumMoves() == 0 && !i->isChecked(b) && i->getCoords() == {c.x - 3, c.y}) { // Check if it has moved yet
+                    pMoves.push_back({{c.x - 2, y}});
                 }
                 break;
             }
