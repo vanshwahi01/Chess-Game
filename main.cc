e2247f3d9ea14f428de04ecee102eebc3280da1a
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2023-12-04 20:45:12 -0500

    Removed addPiece function

--- main.cc
+++ main.cc
@@ -57,44 +57,6 @@ Coordinate blackKCoord(Board &b) {
         }
 }
 
-// void addPiece(const string type, const Coordinate c, const Colour colour, Board &b){
-//         string name;
-//         if(type == "K") { //if we're adding a King   
-//                 King temp {c, colour, *b, "King"};
-//         }
-//         else if(type == "Q"){ // if we're adding a Queen
-//                 Queen temp {c, colour, *b, "Queen"};
-//         }
-//         else if(type == "B") { // if we're adding a Bishop
-//                 Bishop temp {c, colour, *b, "Bishop"};
-//         }
-//         else if(type == "R"){ // if we're adding a Rook
-//                 Rook temp {c, colour, *b, "Rook"};
-//         }
-//         else if(type == "N") { // if we're adding a Knight
-//                 Knight temp {c, colour, *b, "Knight"};
-//         }
-//         else if(type == "P"){ // if we're adding a Pawn
-//                 Pawn temp {c, colour, *b, "Pawn"};
-//         }
-//         else {
-//                 return; // don't do anything if valid type wasn't given
-//         }
-//         //checking if another piece already exists on this coordinate
-//         for(auto p : b->pieces) {
-//                 //if this new piece is being added on a coordinate that already has a piece, new piece replaces old piece
-//                 if(p->getCoords() == c) {
-//                         p = temp;
-//                         //SAHIB GET THE ASSIGNMENT OPERATOR
-//                         //leave function
-//                         return;
-//                 }
-//         }
-//         //adds piece to the vector of pieces
-//         b->getPieces().push_back(temp); //need to change to setPiece cause pieces vector is private
-        
-// }
-
 int main() {
 
         Board b; // this is blank board
@@ -127,9 +89,10 @@ int main() {
                                                 cout << "You can try adding the pawn in a valid position." << endl;
                                         }
                                         else if(piece != "K") {
-                                                addPiece(piece, getCoordinate(position), curPlayer, b);
-                                                if(curPlayer == Colour::White) p1->incTotalPieces();
-                                                else p2->incTotalPieces();
+                                                if(addPiece(piece, getCoordinate(position), curPlayer, b)){}
+                                                        if(curPlayer == Colour::White) p1->incTotalPieces();
+                                                        else p2->incTotalPieces();
+                                                }
                                         }
                                        
                                         else{
