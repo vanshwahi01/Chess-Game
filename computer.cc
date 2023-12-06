c4533a3c08c162ec3e2564e11c013bf6aa3b3015
Vykiial <109167832+Vykiial@users.noreply.github.com>
2023-12-05 19:39:04 -0500

    gonna jump into a car

--- computer.cc
+++ computer.cc
@@ -13,7 +13,7 @@ void Computer::setLevel(int lev){
     level = lev;
 }
 
-Piece* Computer::randomPiece(Board b, Colour curPlayer){//will this give me an error saying totalPieces is not declared in this scope? It technically comes with it because it's a player... but idk
+void Computer::randomPiece(Board& b, Colour curPlayer){//will this give me an error saying totalPieces is not declared in this scope? It technically comes with it because it's a player... but idk
         // srand((unsigned int)time(NULL));
         // int randomIndex = rand() % totalPieces;//gives us a random index from 0 to whatever the total number of pieces is;
         // return randomIndex;
@@ -27,11 +27,11 @@ Piece* Computer::randomPiece(Board b, Colour curPlayer){//will this give me an e
                 if(temp[randomIndex]->getColour() == curPlayer) break; // Keeps getting a random index
                 randomIndex = rand() % totalPieces;
         }
-            
+        Coordinate tempCoords = temp[randomIndex]->getCoords();
         //get random index from possible moves
-        vector<Coordinate> pMoves = temp[randomIndex]->possibleMoves(b, temp[randomIndex], temp[randomIndex]->getCoords());
-        int numPossMoves = temp[randomIndex]->pMoves.size();
+        vector<Coordinate> pMoves = temp[randomIndex]->possibleMoves(b, *temp[randomIndex], tempCoords);
+        int numPossMoves = pMoves().size();
         int possMovesIndex = rand() % numPossMoves;
         
-        b->move(temp[randomIndex]->getCoords() , pMoves[possMovesIndex]);
+        b.move(temp[randomIndex]->getCoords() , pMoves[possMovesIndex]);
 }
