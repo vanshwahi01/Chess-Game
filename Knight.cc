acfe94b9e232bb4753a46f0c2e249c1440e3f1b4
Vykiial <109167832+Vykiial@users.noreply.github.com>
2023-12-05 19:41:57 -0500

    ahhhhh

--- Knight.cc
+++ Knight.cc
@@ -6,44 +6,44 @@ using namespace std;
 Knight::Knight(Coordinate c, Colour colour, Board b, string type) : Piece{c, colour, b, type} {}
 
 vector<Coordinate> Knight::possibleMoves(Board& b, Piece& p, Coordinate& c) {
-    vector<Coordinates> pMoves(0);
+    vector<Coordinate> pMoves(0);
 
-    if(!b.isOccupied({c.x - 2, c.y + 1}) || getPiece({c.x - 2, c.y + 1}).getColour() != p.getColour()) { // 1 up 2 left
+    if(!b.isOccupied({c.x - 2, c.y + 1}) || b.getPiece({c.x - 2, c.y + 1}).getColour() != p.getColour()) { // 1 up 2 left
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x - 2, c.y + 1});
     }
 
-    if(!b.isOccupied({c.x - 1, c.y + 2}) || getPiece({c.x - 1, c.y + 2}).getColour() != p.getColour()) { // 2 up 1 left
+    if(!b.isOccupied({c.x - 1, c.y + 2}) || b.getPiece({c.x - 1, c.y + 2}).getColour() != p.getColour()) { // 2 up 1 left
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x - 1, c.y + 2});
     }
 
-    if(!b.isOccupied({c.x + 1, c.y + 2}) || getPiece({c.x + 1, c.y + 2}).getColour() != p.getColour()) { // 2 up 1 right
+    if(!b.isOccupied({c.x + 1, c.y + 2}) || b.getPiece({c.x + 1, c.y + 2}).getColour() != p.getColour()) { // 2 up 1 right
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x + 1, c.y + 2});
     }
 
-    if(!b.isOccupied({c.x + 2, c.y + 1}) || getPiece({c.x + 2, c.y + 1}).getColour() != p.getColour()) { // 1 up 2 right
+    if(!b.isOccupied({c.x + 2, c.y + 1}) || b.getPiece({c.x + 2, c.y + 1}).getColour() != p.getColour()) { // 1 up 2 right
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x + 2, c.y + 1});
     }
 
-    if(!b.isOccupied({c.x + 2, c.y - 1}) || getPiece({c.x + 2, c.y - 1}).getColour() != p.getColour()) { // 1 down 2 right
+    if(!b.isOccupied({c.x + 2, c.y - 1}) || b.getPiece({c.x + 2, c.y - 1}).getColour() != p.getColour()) { // 1 down 2 right
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x + 2, c.y - 1});
     }
 
-    if(!b.isOccupied({c.x + 1, c.y - 2}) || getPiece({c.x + 1, c.y - 2}).getColour() != p.getColour()) { // 2 down 1 right
+    if(!b.isOccupied({c.x + 1, c.y - 2}) || b.getPiece({c.x + 1, c.y - 2}).getColour() != p.getColour()) { // 2 down 1 right
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x + 1, c.y - 2});
     }
 
-    if(!b.isOccupied({c.x - 1, c.y - 2}) || getPiece({c.x - 1, c.y - 2}).getColour() != p.getColour()) { // 2 down 1 left
+    if(!b.isOccupied({c.x - 1, c.y - 2}) || b.getPiece({c.x - 1, c.y - 2}).getColour() != p.getColour()) { // 2 down 1 left
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x - 1, c.y - 2});
     }
 
-    if(!b.isOccupied({c.x - 2, c.y - 1}) || getPiece({c.x - 2, c.y - 1}).getColour() != p.getColour()) { // 1 down 2 left
+    if(!b.isOccupied({c.x - 2, c.y - 1}) || b.getPiece({c.x - 2, c.y - 1}).getColour() != p.getColour()) { // 1 down 2 left
         // Enters if the space is free or it is occupied by the opposing colour
         pMoves.push_back({c.x - 2, c.y - 1});
     }
