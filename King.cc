4ed7597a5f0894b77ebea469b93f11a48292ff8f
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2023-12-05 19:21:41 -0500

    Fixed King

--- King.cc
+++ King.cc
@@ -28,61 +28,61 @@ bool King::willBeChecked(const Board& b, const Coordinate& c) {
 }
 
 vector<Coordinate> King::possibleMoves(Board& b, Piece& p, Coordinate& c) {
-    vector<Coordinates> pMoves[0];
+    vector<Coordinate> pMoves(0);
 
-    if((c->x - 1 > -1 && c->y + 1 < 8 && b.isOccupied({c->x - 1, c->y + 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x - 1, c->y + 1})) { // Left forward diagnol
-        if(!willBeChecked(b, {c->x - 1, c->y + 1})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x - 1, c->y + 1});
+    if((c.x - 1 > -1 && c.y + 1 < 8 && b.isOccupied({c.x - 1, c.y + 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x - 1, c.y + 1})) { // Left forward diagnol
+        if(!willBeChecked(b, {c.x - 1, c.y + 1})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x - 1, c.y + 1});
         }
     }
 
-    if((c->y + 1 < 8 && b.isOccupied({c->x, c->y + 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x, c->y + 1})) { // Straight ahead
-        if(!willBeChecked(b, {c->x, c->y + 1})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x, c->y + 1});
+    if((c.y + 1 < 8 && b.isOccupied({c.x, c.y + 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x, c.y + 1})) { // Straight ahead
+        if(!willBeChecked(b, {c.x, c.y + 1})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x, c.y + 1});
         }
     }
 
-    if((c->x + 1 < 8 && c->y + 1 < 8 && b.isOccupied({c->x + 1, c->y + 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x + 1, c->y + 1})) { // Right forward diagnol
-        if(!willBeChecked(b, {c->x + 1, c->y + 1})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x + 1, c->y + 1});
+    if((c.x + 1 < 8 && c.y + 1 < 8 && b.isOccupied({c.x + 1, c.y + 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x + 1, c.y + 1})) { // Right forward diagnol
+        if(!willBeChecked(b, {c.x + 1, c.y + 1})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x + 1, c.y + 1});
         }
     }
 
-    if((c->x + 1 < 8 && b.isOccupied({c->x + 1, c->y}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x + 1, c->y})) { // Right horizontal
-        if(!willBeChecked(b, {c->x + 1, c->y})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x + 1, c->y});
+    if((c.x + 1 < 8 && b.isOccupied({c.x + 1, c.y}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x + 1, c.y})) { // Right horizontal
+        if(!willBeChecked(b, {c.x + 1, c.y})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x + 1, c.y});
         }
     }
 
-    if((c->x + 1 < 8 && c->y - 1 > -1 && b.isOccupied({c->x + 1, c->y - 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x + 1, c->y - 1})) { // Right backwards diagnol
-        if(!willBeChecked(b, {c->x + 1, c->y - 1})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x + 1, c->y - 1});
+    if((c.x + 1 < 8 && c.y - 1 > -1 && b.isOccupied({c.x + 1, c.y - 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x + 1, c.y - 1})) { // Right backwards diagnol
+        if(!willBeChecked(b, {c.x + 1, c.y - 1})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x + 1, c.y - 1});
         }
     }
 
-    if((c->y - 1 > -1 && b.isOccupied({c->x, c->y - 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x, c->y - 1})) { // Vertical backwards
-        if(!willBeChecked(b, {c->x, c->y - 1})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x, c->y - 1});
+    if((c.y - 1 > -1 && b.isOccupied({c.x, c.y - 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x, c.y - 1})) { // Vertical backwards
+        if(!willBeChecked(b, {c.x, c.y - 1})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x, c.y - 1});
         }
     }
 
-    if((c->x - 1 > -1 && c->y - 1 > -1 && b.isOccupied({c->x - 1, c->y - 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x - 1, c->y - 1})) { // Left backwards diagnol
-        if(!willBeChecked(b, {c->x - 1, c->y - 1})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x - 1, c->y - 1});
+    if((c.x - 1 > -1 && c.y - 1 > -1 && b.isOccupied({c.x - 1, c.y - 1}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x - 1, c.y - 1})) { // Left backwards diagnol
+        if(!willBeChecked(b, {c.x - 1, c.y - 1})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x - 1, c.y - 1});
         }
     }
 
-    if((c->x - 1 > -1 && b.isOccupied({c->x - 1, c->y}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c->x - 1, c->y})) { // Left horizontal
-        if(!willBeChecked(b, {c->x - 1, c->y})) { // Check if the king will be checked at the new location
-            pMoves.push_back({c->x - 1, c->y});
+    if((c.x - 1 > -1 && b.isOccupied({c.x - 1, c.y}) && b.getPiece.getColour() != p.getColour()) || !b.isOccupied({c.x - 1, c.y})) { // Left horizontal
+        if(!willBeChecked(b, {c.x - 1, c.y})) { // Check if the king will be checked at the new location
+            pMoves.push_back({c.x - 1, c.y});
         }
     }
 
-    if(c->x + 2 < 8 && !b.isOccupied({c->x + 1, y}) && !b.isOccupied({c->x + 2, y}) && p->getNumMoves() == 0) { // Check for castling
+    if(c.x + 2 < 8 && !b.isOccupied({c.x + 1, y}) && !b.isOccupied({c.x + 2, y}) && p->getNumMoves() == 0) { // Check for castling
         for(auto i: b.getPieces()) { // Looking for the correct king
             if(i->getType == "Rook" && i->getColour() == p->getColour()) { // Find the correct King
-                if(i->getNumMoves() == 0 && i->getCoords() == {c->x + 3, c->y}) { // Check if it has moved yet
-                    pMoves.push_back({{c->x + 2, y}});
+                if(i->getNumMoves() == 0 && i->getCoords() == {c.x + 3, c.y}) { // Check if it has moved yet
+                    pMoves.push_back({{c.x + 2, y}});
                 }
                 break;
             }
